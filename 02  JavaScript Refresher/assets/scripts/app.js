d55090fcfb41c64823786d35c4b4fcbe445ffec7
Muhammad Ramzan Akram <ramzanakram1875@outlook.com>
2024-12-19 12:45:27 +0500

    Learn js Import & Export

---
+++ 02  JavaScript Refresher/assets/scripts/app.js
@@ -0,0 +1,30 @@
+//Course Name = The complete React Course 2024 Udemy (Maximilian)
+//Section Name = JavaScript Refresher
+//Date =  19-Dec-2024
+//Video Num & Topic Name = 05. import & export
+//1st way to import (Name Import)
+
+//import { apiKey } from "./utils.js";
+//console.log(apiKey); //Muhammad Ramzan Akram
+
+//2nd way to import (Default Import)
+
+// import apiKey from "./utils.js";
+// console.log(apiKey); //Muhammad Ramzan Akram
+
+//3rd way to import (As per Names)
+
+// import { myName, myDaughterName, myWifeName } from "./utils.js";
+
+// console.log(myName, myWifeName, myDaughterName); //Muhammad Ramzan Akram, Kanwal, Mirha
+
+// //4th way to import (All things)
+
+// import * as utils from "./utils.js";
+
+// console.log(utils.myDaughterName, utils.default); //Mirha, Muhammad Ramzan Akram
+
+// //5th way to import (Alisa Import)
+
+// import { myWifeName as myBestFriend } from "./utils.js";
+// console.log(myBestFriend); //Kanwal
---
+++ 02  JavaScript Refresher/assets/scripts/utils.js
@@ -0,0 +1,13 @@
+//1st way to export
+
+export let apiKey = "Muhammad Ramzan Akram";
+
+//2nd way to export
+
+export default "Muhammad Ramzan Akram";
+
+//3rd way to export
+
+export const myName = "Muhammad Ramzan Akram";
+export const myDaughterName = "Mirha Ramzan";
+export const myWifeName = "Kanwal Shehzadi";
